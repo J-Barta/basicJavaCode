13d5365805b3ac5abc57c9e2b23cdddfd79b527e
J-Barta <53406552+J-Barta@users.noreply.github.com>
2020-07-16 10:26:10 -0400

    Did Tommy's assignment

--- src/com/company/CustomAssignments/TommyResponses.java
+++ src/com/company/CustomAssignments/TommyResponses.java
@@ -1,4 +1,28 @@
 package com.company.CustomAssignments;
 
+import java.util.Scanner;
+
 public class TommyResponses {
+    public static String greeting;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello and welcome to this wonderous program. Please input a greeting.");
+
+        greeting = input.nextLine();
+
+        if(greeting.equalsIgnoreCase("hello")) {
+            System.out.println("Hello!");
+        } else if(greeting.equalsIgnoreCase("hi")) {
+            System.out.println("Hi!");
+        } else if(greeting.equalsIgnoreCase("sup")) {
+            System.out.println("Grammar is good!");
+        } else if(greeting.equalsIgnoreCase("helo")) {
+            System.out.println("HELO FRIEND!");
+        } else if(greeting.equalsIgnoreCase("cheeky gigle")) {
+            System.out.println("gigle gigle!");
+        } else {
+            System.out.println(greeting + "... interesting greeting");
+        }
+    }
 }
